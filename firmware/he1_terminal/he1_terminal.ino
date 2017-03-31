5430ab6f536b2031c619a2b8f624c8ebbbc9e0a8
coon@c-base.org <coon@defekt.localdomain>
2017-04-01 01:26:06 +0200

    implement all LEDs

--- firmware/he1_terminal/he1_terminal.ino
+++ firmware/he1_terminal/he1_terminal.ino
@@ -1,33 +1,38 @@
 /**********************************************************
  *  
  * Author: coon
- * Last maintained: 29th March 2017
+ * Last maintained: 01. April 2017
  * 
  * Description: Controls the LEDs and the switches of the 
  *              HE1-Terminal of c-base.
  *
- * TODO: - Solder remaining LEDs
- *       - Connect the switches
+ * TODO: - Connect the switches
  *       - Find out how the second rotary switch works
  *       - MQTT implementation
  * 
  **********************************************************/
 
-const int numLeds = 2; // Number of LEDs which are currently soldered.
+const int numLeds = 9; // Number of LEDs which are currently soldered.
+  
+void setLeds(uint32_t leds) {
+  auto ledIsSet = [](int leds, int ledNo) -> int {
+    return (leds & (1 << ledNo)) > 0;
+  };
 
-void setLeds(int leds) {
-  digitalWrite(PK_0, leds & (1 << 0));
-  digitalWrite(PK_1, leds & (1 << 1));
-  digitalWrite(PK_2, leds & (1 << 2));
-  digitalWrite(PK_3, leds & (1 << 3));
-  digitalWrite(PK_4, leds & (1 << 4));
-  digitalWrite(PK_5, leds & (1 << 5));
-  digitalWrite(PK_6, leds & (1 << 6));
-  digitalWrite(PK_7, leds & (1 << 7));
-  digitalWrite(PQ_0, leds & (1 << 8));
+  digitalWrite(PK_0, ledIsSet(leds, 0));
+  digitalWrite(PK_1, ledIsSet(leds, 1));
+  digitalWrite(PK_2, ledIsSet(leds, 2));
+  digitalWrite(PK_3, ledIsSet(leds, 3));
+  digitalWrite(PK_4, ledIsSet(leds, 4)); 
+  digitalWrite(PK_5, ledIsSet(leds, 5));
+  digitalWrite(PK_6, ledIsSet(leds, 6));
+  digitalWrite(PK_7, ledIsSet(leds, 7));
+  digitalWrite(PQ_0, ledIsSet(leds, 8));
 }
 
 void setup() {
+  Serial.begin(115200);
+  
   pinMode(PK_0, OUTPUT);
   pinMode(PK_1, OUTPUT);
   pinMode(PK_2, OUTPUT);
@@ -39,10 +44,12 @@ void setup() {
   pinMode(PQ_0, OUTPUT);
 }
 
-void loop() {
+void loop() {  
   for(int i = 0; i < (1 << numLeds); ++i) {
     setLeds(i);
-    delay(1000);
+    delay(125);
+
+    Serial.println(i);
   }
 }
 
